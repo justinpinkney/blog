0da0532dd9b82ce94914296b830c69fd534c6fca
Justin <605492+justinpinkney@users.noreply.github.com>
2023-06-20 01:39:49 +0100

    add copy for redirects

--- eleventy.config.js
+++ eleventy.config.js
@@ -21,6 +21,7 @@ module.exports = function(eleventyConfig) {
 	});
 	eleventyConfig.addPassthroughCopy("**/*.mp4");
 	eleventyConfig.addPassthroughCopy("**/*.jpg");
+	eleventyConfig.addPassthroughCopy("_redirects");
 
 	// Run Eleventy when these files change:
 	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
